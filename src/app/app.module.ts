732c835714cb68de91522e3684dc0c5940528402
Brain Tucker <brian.briantucker@gmail.com>
2017-10-09 14:12:37 -0500

    C:/Program Files/Git/#/albums now redirect to the albums-list.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { routing } from './app.routing';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { AlbumService } from './album/album.service';
 import { ArtistService } from './artist/artist.service';
 import { ArtistListComponent } from './artist/artist-list.component';
 import { ArtistDetailComponent } from './artist/artist-detail.component';
+import { AlbumListComponent } from './album/album-list.component';
+import { AlbumDetailComponent } from './album/album-detail.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule, routing],
   declarations: [
     AppComponent,
     ArtistListComponent,
-    ArtistDetailComponent
+    ArtistDetailComponent,
+    AlbumListComponent,
+    AlbumDetailComponent
   ],
   providers: [
     ArtistService,
+    AlbumService,
     {provide: LocationStrategy, useClass: HashLocationStrategy}
   ],
   bootstrap: [AppComponent]
